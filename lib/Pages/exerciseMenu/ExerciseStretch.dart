bf78f351d683031c60f8e4e49178ba82410164e1
Zyza123 <53822449+Zyza123@users.noreply.github.com>
2023-05-08 18:31:05 +0200

    wersja testowa - list tile i przycisk

--- lib/Pages/exerciseMenu/ExerciseStretch.dart
+++ lib/Pages/exerciseMenu/ExerciseStretch.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
 
-class ExerciseStretch extends StatelessWidget {
+class ExerciseStretch extends StatefulWidget {
   const ExerciseStretch({Key? key}) : super(key: key);
 
   @override
+  _ExerciseStretchState createState() => _ExerciseStretchState();
+}
+
+class _ExerciseStretchState extends State<ExerciseStretch> {
+  @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: EdgeInsets.only(top:20.0),
-      child: Container(
-        width: 200,
-        height: 200,
-        color: Colors.blue,
+    return Expanded(
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: 16,
+            right: 16,
+            child: FloatingActionButton(
+              backgroundColor: const Color(0xFF2E8B57),
+              onPressed: () {
+                // Tutaj dodaj swoją funkcję, która ma zostać wykonana po kliknięciu przycisku
+              },
+              child: Icon(Icons.add),
+            ),
+          ),
+          CustomScrollView(
+            slivers: [
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text('Item $index'),
+                    );
+                  },
+                  childCount: 5,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
