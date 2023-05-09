99bbf9fcce93874b6a4d60e75ce558a6b2dfcf1e
Zyza123 <53822449+Zyza123@users.noreply.github.com>
2023-05-09 13:39:15 +0200

    Rozwijany kafelek, lekki styl
    Dodano rozwijanie kafelka, jakie dane będą w kafelku, delikatnie wystylizowane

--- lib/Pages/exerciseMenu/ExerciseStretch.dart
+++ lib/Pages/exerciseMenu/ExerciseStretch.dart
@@ -8,12 +8,12 @@ class ExerciseStretch extends StatefulWidget {
 }
 
 class _ExerciseStretchState extends State<ExerciseStretch> {
-
   int _itemCount = 1;
   int _selectedIndex = -1;
 
   @override
   Widget build(BuildContext context) {
+
     return Expanded(
       child: Stack(
         children: [
@@ -26,27 +26,105 @@ class _ExerciseStretchState extends State<ExerciseStretch> {
                   physics: const NeverScrollableScrollPhysics(),
                   itemCount: _itemCount,
                   itemBuilder: (BuildContext context, int index) {
-                    final backgroundColor =
-                    _selectedIndex == index ? Colors.green.shade100 : Colors.white;
+                    final bool showMore = _selectedIndex == index
+                        ? true : false;
                     return Container(
-                      margin: EdgeInsets.symmetric(horizontal: 10),
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
                       decoration: BoxDecoration(
-                        color: backgroundColor,
+                        color: Colors.white,
                         borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: const Color(0xFF2E8B57),
+                          width: 2.0,style: BorderStyle.solid)
                       ),
-                      child: ListTile(
-                        onTap: (){
-                          setState(() {
-                            _selectedIndex = index;
-                            print("kliknieto!");
-                          });
-                        },
-                        leading: Icon(Icons.person),
-                        title: Text('This is: $index'),
-                        subtitle: Text('Subtitle for title...'),
-                        trailing: Icon(Icons.arrow_drop_down_sharp),
-                        dense: false,
-                        isThreeLine: false,
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: ListTile(
+                          onTap: () {
+                            setState(() {
+                              if (_selectedIndex == index) {
+                                // Ten sam element został kliknięty po raz drugi, więc go odznaczamy
+                                _selectedIndex = -1;
+                              } else {
+                                // Inny element został kliknięty, więc go zaznaczamy
+                                _selectedIndex = index;
+                              }
+                              print("kliknieto!");
+                            });
+                          },
+                          leading: const Icon(Icons.person, size: 32),
+                          title: Container(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Nazwa cwiczenia: $index',
+                                  style: const TextStyle(fontSize: 20,
+                                      color:  Color(0xFF2E8B57),
+                                      fontWeight: FontWeight.bold),),
+                                const SizedBox(height: 15),
+                                Row(
+                                  children: [
+                                    Expanded(
+                                        flex: 1,
+                                      child: RichText(
+                                        text: TextSpan(
+                                          children: [
+                                            const TextSpan(
+                                              text: 'Serie: ',
+                                              style: TextStyle(
+                                                fontSize: 16,
+                                                color: Color(0xFF2E8B57),
+                                                fontWeight: FontWeight.bold
+                                              ),
+                                            ),
+                                            TextSpan(
+                                              text: '$index',
+                                              style: const TextStyle(
+                                                fontSize: 16,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                        flex: 1,
+                                        child: RichText(
+                                    text: TextSpan(
+                                    children: [
+                                    const TextSpan(
+                                      text: 'Powtórzenia: ',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: Color(0xFF2E8B57),
+                                        fontWeight: FontWeight.bold
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: '$index',
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                          ),
+                                    ),
+                                  ],),
+                                if (showMore) const SizedBox(height: 10),
+                                if (showMore)const Text("To jest dodatkowy napis\n"
+                                    "Tu jest druga linijka tego napisu\n"
+                                    "Tu jest trzecia i ostatnia linijka",
+                                    style: TextStyle(fontSize: 16),)
+                              ],
+                            ),
+                          ),
+                          trailing: showMore == true ?
+                          const Icon(Icons.arrow_drop_up_sharp) : const Icon(Icons.arrow_drop_down_sharp),
+                          dense: false,
+                          isThreeLine: false,
+                        ),
                       ),
                     );
                   },
