befd6c8e859c2d60675ebdf7f0f412c8f40bbb42
Zyza123 <53822449+Zyza123@users.noreply.github.com>
2023-05-10 13:41:02 +0200

    modyfikacja listy
    Dodano listę do której dodają się elementy i automatycznie się aktualizuje z listą listView z CSV.
    Oprócz tego dodano mechanizm usuwania elementu poprzez przesunięcie w lewo lub prawo.

--- lib/Pages/exerciseMenu/ExerciseStretch.dart
+++ lib/Pages/exerciseMenu/ExerciseStretch.dart
@@ -8,12 +8,11 @@ class ExerciseStretch extends StatefulWidget {
 }
 
 class _ExerciseStretchState extends State<ExerciseStretch> {
-  int _itemCount = 1;
   int _selectedIndex = -1;
+  final items = List<String>.generate(0, (i) => '${i + 1}');
 
   @override
   Widget build(BuildContext context) {
-
     return Expanded(
       child: Stack(
         children: [
@@ -24,106 +23,129 @@ class _ExerciseStretchState extends State<ExerciseStretch> {
                   separatorBuilder: (context, index) => SizedBox(height: 10),
                   shrinkWrap: true,
                   physics: const NeverScrollableScrollPhysics(),
-                  itemCount: _itemCount,
+                  itemCount: items.length,
                   itemBuilder: (BuildContext context, int index) {
-                    final bool showMore = _selectedIndex == index
-                        ? true : false;
-                    return Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 10),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: const Color(0xFF2E8B57),
-                          width: 2.0,style: BorderStyle.solid)
+                    final bool showMore =
+                        _selectedIndex == index ? true : false;
+                    final item = items[index];
+                    return Dismissible(
+                      key: Key(item),
+                      onDismissed: (direction) {
+                        setState(() {
+                          items.removeAt(index);
+                        });
+                      },
+                      background: Container(
+                        color: Colors.red,
+                        child: const Icon(Icons.cancel),
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: ListTile(
-                          onTap: () {
-                            setState(() {
-                              if (_selectedIndex == index) {
-                                // Ten sam element został kliknięty po raz drugi, więc go odznaczamy
-                                _selectedIndex = -1;
-                              } else {
-                                // Inny element został kliknięty, więc go zaznaczamy
-                                _selectedIndex = index;
-                              }
-                              print("kliknieto!");
-                            });
-                          },
-                          leading: const Icon(Icons.person, size: 32),
-                          title: Container(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Nazwa cwiczenia: $index',
-                                  style: const TextStyle(fontSize: 20,
-                                      color:  Color(0xFF2E8B57),
-                                      fontWeight: FontWeight.bold),),
-                                const SizedBox(height: 15),
-                                Row(
-                                  children: [
-                                    Expanded(
-                                        flex: 1,
-                                      child: RichText(
-                                        text: TextSpan(
-                                          children: [
-                                            const TextSpan(
-                                              text: 'Serie: ',
-                                              style: TextStyle(
-                                                fontSize: 16,
-                                                color: Color(0xFF2E8B57),
-                                                fontWeight: FontWeight.bold
-                                              ),
-                                            ),
-                                            TextSpan(
-                                              text: '$index',
-                                              style: const TextStyle(
-                                                fontSize: 16,
-                                                color: Colors.black,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                    Expanded(
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                                color: const Color(0xFF2E8B57),
+                                width: 2.0,
+                                style: BorderStyle.solid)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: ListTile(
+                            onTap: () {
+                              setState(() {
+                                if (_selectedIndex == index) {
+                                  // Ten sam element został kliknięty po raz drugi, więc go odznaczamy
+                                  _selectedIndex = -1;
+                                } else {
+                                  // Inny element został kliknięty, więc go zaznaczamy
+                                  _selectedIndex = index;
+                                }
+                                print("kliknieto!");
+                              });
+                            },
+                            leading: const Icon(Icons.sports_gymnastics, size: 32,
+                              color: Color(0xFFEC9006),),
+                            title: Container(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Nazwa cwiczenia: ${items[index]}',
+                                    style: const TextStyle(
+                                        fontSize: 20,
+                                        color: Color(0xFF2E8B57),
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  const SizedBox(height: 15),
+                                  Row(
+                                    children: [
+                                      Expanded(
                                         flex: 1,
                                         child: RichText(
-                                    text: TextSpan(
-                                    children: [
-                                    const TextSpan(
-                                      text: 'Powtórzenia: ',
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        color: Color(0xFF2E8B57),
-                                        fontWeight: FontWeight.bold
+                                          text: TextSpan(
+                                            children: [
+                                              const TextSpan(
+                                                text: 'Serie: ',
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    color: Color(0xFF2E8B57),
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                              TextSpan(
+                                                text: '${items[index]}',
+                                                style: const TextStyle(
+                                                  fontSize: 16,
+                                                  color: Colors.black,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
                                       ),
-                                    ),
-                                    TextSpan(
-                                      text: '$index',
-                                      style: const TextStyle(
-                                        fontSize: 16,
-                                        color: Colors.black,
+                                      Expanded(
+                                        flex: 1,
+                                        child: RichText(
+                                          text: TextSpan(
+                                            children: [
+                                              const TextSpan(
+                                                text: 'Powtórzenia: ',
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    color: Color(0xFF2E8B57),
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                              TextSpan(
+                                                text: '${items[index]}',
+                                                style: const TextStyle(
+                                                  fontSize: 16,
+                                                  color: Colors.black,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
                                       ),
-                                    ),
-                                  ],
-                                ),
-                          ),
-                                    ),
-                                  ],),
-                                if (showMore) const SizedBox(height: 10),
-                                if (showMore)const Text("To jest dodatkowy napis\n"
-                                    "Tu jest druga linijka tego napisu\n"
-                                    "Tu jest trzecia i ostatnia linijka",
-                                    style: TextStyle(fontSize: 16),)
-                              ],
+                                    ],
+                                  ),
+                                  if (showMore) const SizedBox(height: 10),
+                                  if (showMore)
+                                    const Text(
+                                      "To jest dodatkowy napis\n"
+                                      "Tu jest druga linijka tego napisu\n"
+                                      "Tu jest trzecia i ostatnia linijka",
+                                      style: TextStyle(fontSize: 16),
+                                    )
+                                ],
+                              ),
                             ),
+                            trailing: showMore == true
+                                ? const Icon(Icons.arrow_drop_up_sharp)
+                                : const Icon(Icons.arrow_drop_down_sharp),
+                            dense: false,
+                            isThreeLine: false,
                           ),
-                          trailing: showMore == true ?
-                          const Icon(Icons.arrow_drop_up_sharp) : const Icon(Icons.arrow_drop_down_sharp),
-                          dense: false,
-                          isThreeLine: false,
                         ),
                       ),
                     );
@@ -144,7 +166,7 @@ class _ExerciseStretchState extends State<ExerciseStretch> {
               onPressed: () {
                 print('Kliknięto!');
                 setState(() {
-                  _itemCount++;
+                  items.add("${items.length + 1}");
                 });
                 // Tutaj dodaj swoją funkcję, która ma zostać wykonana po kliknięciu przycisku
               },
