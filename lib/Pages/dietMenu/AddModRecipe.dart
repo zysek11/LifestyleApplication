466eb511c6ccbf7cce4b2ff3d679b3d2114abaf4
Zyza123 <53822449+Zyza123@users.noreply.github.com>
2023-06-06 19:03:44 +0200

    2 czesc dodawania nowych rzeczy
    Dalsze dodawanie nowych rzeczy, teraz do tego okna pokazuje się opcja dawania generowanych składników które dodają się do listy, potem będzie można dodać ja tak do hive.

--- lib/Pages/dietMenu/AddModRecipe.dart
+++ lib/Pages/dietMenu/AddModRecipe.dart
@@ -8,6 +8,8 @@ class AddModRecipe extends StatefulWidget {
 }
 
 class _AddModRecipeState extends State<AddModRecipe> {
+  var ingredientList = <String>[];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,23 +19,65 @@ class _AddModRecipeState extends State<AddModRecipe> {
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children:  [
-              Image(
-                image: AssetImage('assets/images/wiesiek.png'),
-                width: MediaQuery.of(context).size.width, // Szerokość całego ekranu
-                height: MediaQuery.of(context).size.height * 0.3, // 0.3 wysokości ekranu
-                fit: BoxFit.cover, // Dopasowanie obrazu do rozmiaru
-              ),
+          children: [
+            Image(
+              image: AssetImage('assets/images/wiesiek.png'),
+              width: MediaQuery.of(context)
+                  .size
+                  .width, // Szerokość całego ekranu
+              height: MediaQuery.of(context).size.height *
+                  0.3, // 0.3 wysokości ekranu
+              fit: BoxFit.cover, // Dopasowanie obrazu do rozmiaru
+            ),
             Padding(
               padding: const EdgeInsets.all(15),
               child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  NameCont(),
-                  TextFieldBlack(),
+                  NameCont(name: "Nazwa"),
+                  TextFieldBlack(
+                    hintTF: "Podaj nazwe przepisu...",
+                  ),
+                  SizedBox(height: 20),
+                  NameCont(name: "Składniki"),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: ingredientList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Row(
+                        children: [
+                          Expanded(
+                            child: TextFieldBlack(
+                              hintTF: "Podaj składnik...",
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.cancel,color: Color(0xFF2E8B57),),
+                            onPressed: () {
+                              setState(() {
+                                ingredientList.removeAt(index);
+                              });
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF2E8B57), // Kolor tła przycisku
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        ingredientList.add("");
+                      });
+                    },
+                    child: Text("Dodaj składnik"),
+                  ),
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
@@ -42,36 +86,42 @@ class _AddModRecipeState extends State<AddModRecipe> {
 }
 
 class TextFieldBlack extends StatelessWidget {
+  final String hintTF;
+
   const TextFieldBlack({
     Key? key,
+    required this.hintTF,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-      style: TextStyle(color: Colors.black, fontSize: 20), // Kolor czarny dla tekstu
+      style: TextStyle(color: Colors.black, fontSize: 20),
       decoration: InputDecoration(
-        hintText: "podaj nazwę swojego przepisu",
+        hintText: hintTF,
+        // Wykorzystanie argumentu hintTF jako tekst podpowiedzi
         focusedBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: Colors.black), // Kolor czarny dla dolnego bordera
+          borderSide: BorderSide(color: Color(0xFF2E8B57)),
         ),
       ),
     );
   }
 }
 
-
-
 class NameCont extends StatelessWidget {
-  const NameCont({Key? key}) : super(key: key);
+  final String name;
+  const NameCont({
+    Key? key,
+    required this.name,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 5),
-      child: const Text(
-        'Nazwa',
-        style: TextStyle(
+      child: Text(
+        name,
+        style: const TextStyle(
             fontSize: 26,
             color: Color(0xFF2E8B57),
             fontWeight: FontWeight.bold),
