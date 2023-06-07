987e71d7b1d6767d1ceed719cf2d2ab796e2b35a
Zyza123 <53822449+Zyza123@users.noreply.github.com>
2023-06-07 21:41:14 +0200

    uzupelnienie reszty dodawania
    Dodano inne przykladowe zdjecie plus2, ponadto dodano opis i trochę posprzątano.

--- lib/Pages/dietMenu/AddModRecipe.dart
+++ lib/Pages/dietMenu/AddModRecipe.dart
@@ -32,88 +32,109 @@ class _AddModRecipeState extends State<AddModRecipe> {
         backgroundColor: const Color(0xFF2E8B57),
         title: const Text("Add or modify recipe"),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            GestureDetector(
-              onTap: _pickImage,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.3,
-                decoration: BoxDecoration(
-                  image: isPicked == true
-                      ? DecorationImage(
-                    image: FileImage(pickedImage!),
-                    fit: BoxFit.cover,
-                  )
-                      : const DecorationImage(
-                    image: AssetImage('assets/images/wiesiek.png'),
-                    fit: BoxFit.cover,
+      body: Padding(
+        padding: const EdgeInsets.only(bottom: 8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GestureDetector(
+                onTap: _pickImage,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  decoration: BoxDecoration(
+                    image: isPicked == true
+                        ? DecorationImage(
+                      image: FileImage(pickedImage!),
+                      fit: BoxFit.cover,
+                    )
+                        : const DecorationImage(
+                      image: AssetImage('assets/images/plus2.png'),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  NameCont(name: "Nazwa",verticalPadding: 5),
-                  TextFieldBlack(
-                    hintTF: "Podaj nazwe przepisu...",
-                  ),
-                  SizedBox(height: 20),
-                  NameCont(name: "Składniki",),
-                  ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: ingredientList.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Row(
-                        children: [
-                          Expanded(
-                            child: TextFieldBlack(
-                              hintTF: "Podaj składnik...",
-                            ),
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.cancel, color: Color(0xFF2E8B57)),
-                            onPressed: () {
-                              setState(() {
-                                ingredientList.removeAt(index);
-                              });
-                            },
-                          ),
-                        ],
-                      );
-                    },
-                  ),
-                  SizedBox(height: 10),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor:
-                      const Color(0xFF2E8B57), // Kolor tła przycisku
+              Padding(
+                padding: const EdgeInsets.only(left: 15,top: 15,right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    NameCont(name: "Nazwa",verticalPadding: 5),
+                    TextFieldBlack(
+                      hintTF: "Podaj nazwe przepisu...",
+                      maxLength: 50,
                     ),
-                    onPressed: () {
-                      setState(() {
-                        ingredientList.add("");
-                      });
-                    },
-                    child: Text("Dodaj składnik"),
-                  ),
-                  SizedBox(height: 20),
-                  NameCont(name: "Kalorie i makroskładniki"),
-                  MacroRow(carbohydratesHint: "Podaj kalorie", unitText: "kcal",),
-                  SizedBox(height: 10),
-                  MacroRow(carbohydratesHint: "Podaj weglowodany", unitText: "gram",),
-                  SizedBox(height: 10),
-                  MacroRow(carbohydratesHint: "Podaj tluszcze", unitText: "gram",),
-                  SizedBox(height: 10),
-                  MacroRow(carbohydratesHint: "Podaj bialko", unitText: "bialko",),
-                ],
+                    SizedBox(height: 20),
+                    NameCont(name: "Składniki",),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: ingredientList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Row(
+                          children: [
+                            Expanded(
+                              child: TextFieldBlack(
+                                hintTF: "Podaj składnik...",
+                              ),
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.cancel, color: Color(0xFF2E8B57)),
+                              onPressed: () {
+                                setState(() {
+                                  ingredientList.removeAt(index);
+                                });
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                        const Color(0xFF2E8B57), // Kolor tła przycisku
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          ingredientList.add("");
+                        });
+                      },
+                      child: Text("Dodaj składnik"),
+                    ),
+                    SizedBox(height: 20),
+                    NameCont(name: "Kalorie i makroskładniki"),
+                    MacroRow(carbohydratesHint: "Podaj kalorie", unitText: "kcal", maxLength: 4),
+                    SizedBox(height: 10),
+                    MacroRow(carbohydratesHint: "Podaj weglowodany", unitText: "gram", maxLength: 4),
+                    SizedBox(height: 10),
+                    MacroRow(carbohydratesHint: "Podaj tluszcze", unitText: "gram", maxLength: 4),
+                    SizedBox(height: 10),
+                    MacroRow(carbohydratesHint: "Podaj bialko", unitText: "gram", maxLength: 4),
+                    SizedBox(height: 20),
+                    NameCont(name: "Opis"),
+                    TextFieldBlack(hintTF: "Opis przepisu...",maxLines: 5,),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size.fromHeight(40),
+                        backgroundColor:
+                        const Color(0xFF2E8B57), // Kolor tła przycisku
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          ingredientList.add("");
+                        });
+                      },
+                      child: Text("Dodaj przepis", style: TextStyle(fontSize: 18),),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -123,11 +144,13 @@ class _AddModRecipeState extends State<AddModRecipe> {
 class MacroRow extends StatelessWidget {
   final String carbohydratesHint;
   final String unitText;
+  final int maxLength;
 
   const MacroRow({
     Key? key,
     required this.carbohydratesHint,
     required this.unitText,
+    required this.maxLength
   }) : super(key: key);
 
   @override
@@ -135,9 +158,9 @@ class MacroRow extends StatelessWidget {
     return Row(
       children: [
         Expanded(
-          flex: 5,
+          flex: 2,
           child: Container(
-            child: TextFieldBlack(hintTF: carbohydratesHint),
+            child: TextFieldBlack(hintTF: carbohydratesHint,maxLength: maxLength,),
           ),
         ),
         Expanded(
@@ -150,6 +173,10 @@ class MacroRow extends StatelessWidget {
             ),
           ),
         ),
+        Expanded(
+          flex: 1,
+          child: Container(), // Puste miejsce
+        ),
       ],
     );
   }
@@ -157,19 +184,28 @@ class MacroRow extends StatelessWidget {
 
 class TextFieldBlack extends StatelessWidget {
   final String hintTF;
-  final double verticalPadding; // Nowy argument
+  final double verticalPadding;
+  final int maxLength;
+  final int maxLines;
+  final int minLines;
 
   const TextFieldBlack({
     Key? key,
     required this.hintTF,
-    this.verticalPadding = 0.0, // Domyślna wartość dla wypełnienia pionowego
+    this.verticalPadding = 0.0,
+    this.maxLength = 400,
+    this.maxLines = 1,
+    this.minLines = 1,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: verticalPadding), // Zastosowanie wypełnienia pionowego
+      padding: EdgeInsets.symmetric(vertical: verticalPadding),
       child: TextField(
+        maxLength: maxLength,
+        maxLines: maxLines,
+        minLines: minLines,
         style: TextStyle(color: Colors.black, fontSize: 20),
         decoration: InputDecoration(
           hintText: hintTF,
