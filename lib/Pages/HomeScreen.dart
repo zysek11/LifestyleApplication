9e48d261ba012e281339c7f22ff9764888ce9f43
Zyza123 <53822449+Zyza123@users.noreply.github.com>
2023-06-24 12:54:39 +0200

    wstepne dodawanie danych do home

--- lib/Pages/HomeScreen.dart
+++ lib/Pages/HomeScreen.dart
@@ -6,14 +6,42 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(10.0),
+      width: double.infinity,
       color: Colors.grey.shade200,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('home page'),
-          ],
-        ),
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 10),
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Witaj!',
+              style: TextStyle(fontSize: 25),
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            width: double.infinity,
+            child: Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0), // Zaokrąglenie rogów
+              ),// Wysokość cienia
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Text('Czy wiesz, że ...'),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Text('Tutaj możesz wpisać cokolwiek'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
